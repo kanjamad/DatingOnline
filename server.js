83d3b95956aa1a4589ff7e518eaa7e9a867a651f
kanjamad bosel <kanjamadpor@gmail.com>
2019-07-27 01:25:12 -0700

    initial deployment

--- server.js
+++ server.js
@@ -29,5 +29,5 @@ app.get('/contact', (req,res) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`'Server is running on port' ${PORT}`);
+    console.log(`Server is running on port  ${PORT}`);
 });
